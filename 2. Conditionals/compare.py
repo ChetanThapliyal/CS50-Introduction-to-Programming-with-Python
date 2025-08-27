f8ef8a8f0ff858d53aa2855aa98bab110a3e3da1
Chetan Thapliyal <chetan.thapliyal@outlook.com>
2025-08-27 05:14:14 +0000

    L2

---
+++ 2. Conditionals/compare.py
@@ -0,0 +1,9 @@
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+if x<y:
+    print("x is less than y")
+if x> y:
+    print("x is greater than y")
+if x==y:
+    print("x is equal to y")
