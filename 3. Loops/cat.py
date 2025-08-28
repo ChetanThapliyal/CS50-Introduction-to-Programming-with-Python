5173cf783370236734652873598c17a2f7d68a4d
Chetan Thapliyal <chetan.thapliyal@outlook.com>
2025-08-28 14:45:41 +0000

    Meowing my way to loops :)

--- 3. Loops/cat.py
+++ 3. Loops/cat.py
@@ -1,41 +1,42 @@
-# print("meow")
-# print("meow")
-# print("meow")
+print("meow")
+print("meow")
+print("meow")
 
-# i = 3
-# while i != 0:
-#     print("meow")
-#     i = i - 1
-
-# i = 0
-# while i < 3:
-#     print("meow")
-#     i = i + 1
-
-# for i in [0, 1, 2]:
-#     print("meow")
-
-# for i in range(3):
-#     print("meow")
-
-# for _ in range(3):
-#     print("meow")
-
-# print("meow\n"*3, end="")
-
-# while True:
-#     n = int(input("Enter value for n: "))
-#     if n < 0:
-#         continue
-#     else:
-#         break
-
-# while True:
-#     n = int(input("Enter value for n: "))
-#     if n > 0:
-#         break
-# for _ in range(n):
-#     print("meow")
+# ---
+i = 3
+while i != 0:
+    print("meow")
+    i = i - 1
+# ---
+i = 0
+while i < 3:
+    print("meow")
+    i = i + 1
+# ---
+for i in [0, 1, 2]:
+    print("meow")
+# ---
+for i in range(3):
+    print("meow")
+# ---
+for _ in range(3):
+    print("meow")
+# ---
+print("meow\n"*3, end="")
+# ---
+while True:
+    n = int(input("Enter value for n: "))
+    if n < 0:
+        continue
+    else:
+        break
+# ---
+while True:
+    n = int(input("Enter value for n: "))
+    if n > 0:
+        break
+for _ in range(n):
+    print("meow")
 
 def main():
     meow(3)
@@ -45,7 +46,18 @@ def get_number():
         n = int(input("Enter value for n: "))
         if n > 0:
             break
-
+# ---
 def meow(n):
     for _ in range(n):
         print("meow")
+
+
+
+# camelcase = input("camelcase: ")
+
+# for letter in camelcase:
+#     if letter.isupper():
+#         print("_" + letter.lower(), end="")
+#     else:
+#         print(letter, end="")
+# print()
